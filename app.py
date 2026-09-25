1033134ffd23d553c8e2d7d8ec6e032df2aeb175
Srushti Makwana <srushtimakwana2106@gmail.com>
2026-09-25 12:11:29 +0530

    Create cloud technology PaaS website

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Hello from PaaS!"
+    return render_template("index.html")
 
-app.run(host="0.0.0.0", port=8080)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
